af793a5534cb449f838fee12a67f47b9753a763f
harshit-splunk <hvaghani@splunk.com>
2022-08-04 17:16:06 +0530

    initial commit

---
+++ factory.go
@@ -0,0 +1,39 @@
+package k8sobjectreceiver
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/config"
+	"go.opentelemetry.io/collector/consumer"
+)
+
+const (
+	typeStr = "objects"
+)
+
+func NewFactory() component.ReceiverFactory {
+	return component.NewReceiverFactory(
+		typeStr,
+		createDefaultConfig,
+		component.WithLogsReceiver(
+			createLogsReceiver,
+		),
+	)
+}
+
+func createDefaultConfig() config.Receiver {
+	return &Config{
+		ReceiverSettings: config.NewReceiverSettings(config.NewComponentID(typeStr)),
+	}
+}
+
+func createLogsReceiver(
+	_ context.Context,
+	params component.ReceiverCreateSettings,
+	cfg config.Receiver,
+	consumer consumer.Logs,
+) (component.LogsReceiver, error) {
+	rcfg := cfg.(*Config)
+	return newReceiver(params, rcfg, consumer)
+}
