33035bf97602dd101fa85703ed3e26740fc3b22f
harshit-splunk <hvaghani@splunk.com>
2022-09-08 18:33:45 +0530

    added logic to convert object to logdata

--- receiver.go
+++ receiver.go
@@ -2,15 +2,11 @@ package k8sobjectreceiver
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
-	"go.opentelemetry.io/collector/pdata/pcommon"
-	"go.opentelemetry.io/collector/pdata/plog"
 	"go.uber.org/zap"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/dynamic"
@@ -92,17 +88,15 @@ func (kr *k8sobjectreceiver) startPull(config *K8sObjectsConfig, resource dynami
 	for {
 		select {
 		case <-ticker.C:
-			// pull object
 			objects, err := resource.List(kr.ctx, metav1.ListOptions{
 				FieldSelector: config.FieldSelector,
 				LabelSelector: config.LabelSelector,
 			})
 			if err != nil {
 				kr.setting.Logger.Error("error in pulling object", zap.String("resource", config.gvr.String()), zap.Error(err))
-				// return
 			} else {
-				// feed info
-				fmt.Println(objects)
+				logs := unstructuredListToLogData(objects)
+				kr.consumer.ConsumeLogs(kr.ctx, logs)
 			}
 		case <-kr.ctx.Done():
 			return
@@ -118,11 +112,7 @@ func (kr *k8sobjectreceiver) startWatch(config *K8sObjectsConfig, resource dynam
 		FieldSelector: config.FieldSelector,
 		LabelSelector: config.LabelSelector,
 	})
-	ss, ok := err.(errors.APIStatus)
-	if ok {
-		kr.setting.Logger.Error("error in watching object", zap.String("resource", config.gvr.String()), zap.Any("status", ss.Status().Reason))
-		return
-	} else if err != nil {
+	if err != nil {
 		kr.setting.Logger.Error("error in watching object", zap.String("resource", config.gvr.String()), zap.Error(err))
 		return
 	}
@@ -132,167 +122,15 @@ func (kr *k8sobjectreceiver) startWatch(config *K8sObjectsConfig, resource dynam
 		select {
 		case data := <-res:
 			udata := data.Object.(*unstructured.Unstructured)
-			udata.GetResourceVersion()
-			fmt.Println(udata)
-			// kr.setting.Logger.Info(fmt.Sprint(data.Object))
+			kr.setting.Logger.Info(udata.GetName())
+			logs := unstructuredToLogData(udata)
+			kr.consumer.ConsumeLogs(kr.ctx, logs)
 		case <-kr.ctx.Done():
 			watch.Stop()
 			return
 		}
 	}
-	// client := kr.client
 
-	// res, _ := client.Resource(*config.gvr).Watch(kr.ctx, metav1.ListOptions{})
-	// defer res.Stop()
-	// data := <-res.ResultChan()
-	// udata := data.Object.(*unstructured.Unstructured)
-	// // res.Items
-	// fmt.Println(udata)
-
-}
-
-func (kr *k8sobjectreceiver) toLogData(event *unstructured.UnstructuredList) plog.Logs {
-	out := plog.NewLogs()
-	rls := out.ResourceLogs().AppendEmpty()
-	// resource := rls.Resource()
-	// resource.Attributes().Insert()
-	sl := rls.ScopeLogs().AppendEmpty()
-	logSlice := sl.LogRecords()
-	logSlice.EnsureCapacity(len(event.Items))
-	for _, e := range event.Items {
-		// bytes, err := json.Marshal(e.Object)
-		// if err != nil {
-		// 	kr.setting.Logger.Error("Error in unmarshalling", zap.Error(err))
-		// 	continue
-		// }
-		record := logSlice.AppendEmpty()
-		dest := record.Body()
-		// record.Body().SetStringVal(string(bytes))
-		toAttributeMap(e.Object).CopyTo(dest)
-	}
-	if logSlice.Len() == 0 {
-		kr.setting.Logger.Info(fmt.Sprint("Empty log record generated", event))
-	}
-	return out
-}
-
-func toAttributeMap(obsMap map[string]interface{}) pcommon.Value {
-	attVal := pcommon.NewValueMap()
-	attMap := attVal.MapVal()
-	insertToAttributeMap(obsMap, attMap)
-	return attVal
-}
-
-func insertToAttributeMap(obsMap map[string]interface{}, dest pcommon.Map) {
-	dest.EnsureCapacity(len(obsMap))
-	for k, v := range obsMap {
-		switch t := v.(type) {
-		case bool:
-			dest.InsertBool(k, t)
-		case string:
-			dest.InsertString(k, t)
-		case []string:
-			arr := toStringArray(t)
-			dest.Insert(k, arr)
-		case []byte:
-			dest.InsertBytes(k, pcommon.NewImmutableByteSlice(t))
-		case int64:
-			dest.InsertInt(k, t)
-		case int32:
-			dest.InsertInt(k, int64(t))
-		case int16:
-			dest.InsertInt(k, int64(t))
-		case int8:
-			dest.InsertInt(k, int64(t))
-		case int:
-			dest.InsertInt(k, int64(t))
-		case uint64:
-			dest.InsertInt(k, int64(t))
-		case uint32:
-			dest.InsertInt(k, int64(t))
-		case uint16:
-			dest.InsertInt(k, int64(t))
-		case uint8:
-			dest.InsertInt(k, int64(t))
-		case uint:
-			dest.InsertInt(k, int64(t))
-		case float64:
-			dest.InsertDouble(k, t)
-		case float32:
-			dest.InsertDouble(k, float64(t))
-		case map[string]interface{}:
-			subMap := toAttributeMap(t)
-			dest.Insert(k, subMap)
-		case []interface{}:
-			arr := toAttributeArray(t)
-			dest.Insert(k, arr)
-		default:
-			dest.InsertString(k, fmt.Sprintf("%v", t))
-		}
-	}
-}
-
-func toAttributeArray(obsArr []interface{}) pcommon.Value {
-	arrVal := pcommon.NewValueSlice()
-	arr := arrVal.SliceVal()
-	arr.EnsureCapacity(len(obsArr))
-	for _, v := range obsArr {
-		insertToAttributeVal(v, arr.AppendEmpty())
-	}
-	return arrVal
-}
-
-func insertToAttributeVal(value interface{}, dest pcommon.Value) {
-	switch t := value.(type) {
-	case bool:
-		dest.SetBoolVal(t)
-	case string:
-		dest.SetStringVal(t)
-	case []string:
-		toStringArray(t).CopyTo(dest)
-	case []byte:
-		dest.SetBytesVal(pcommon.NewImmutableByteSlice(t))
-	case int64:
-		dest.SetIntVal(t)
-	case int32:
-		dest.SetIntVal(int64(t))
-	case int16:
-		dest.SetIntVal(int64(t))
-	case int8:
-		dest.SetIntVal(int64(t))
-	case int:
-		dest.SetIntVal(int64(t))
-	case uint64:
-		dest.SetIntVal(int64(t))
-	case uint32:
-		dest.SetIntVal(int64(t))
-	case uint16:
-		dest.SetIntVal(int64(t))
-	case uint8:
-		dest.SetIntVal(int64(t))
-	case uint:
-		dest.SetIntVal(int64(t))
-	case float64:
-		dest.SetDoubleVal(t)
-	case float32:
-		dest.SetDoubleVal(float64(t))
-	case map[string]interface{}:
-		toAttributeMap(t).CopyTo(dest)
-	case []interface{}:
-		toAttributeArray(t).CopyTo(dest)
-	default:
-		dest.SetStringVal(fmt.Sprintf("%v", t))
-	}
-}
-
-func toStringArray(strArr []string) pcommon.Value {
-	arrVal := pcommon.NewValueSlice()
-	arr := arrVal.SliceVal()
-	arr.EnsureCapacity(len(strArr))
-	for _, v := range strArr {
-		insertToAttributeVal(v, arr.AppendEmpty())
-	}
-	return arrVal
 }
 
 // Start ticking immediately.
---
+++ unstructured_to_logdata.go
@@ -0,0 +1,156 @@
+package k8sobjectreceiver
+
+import (
+	"fmt"
+
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/plog"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func unstructuredToLogData(event *unstructured.Unstructured) plog.Logs {
+	out := plog.NewLogs()
+	rls := out.ResourceLogs().AppendEmpty()
+	sl := rls.ScopeLogs().AppendEmpty()
+	logSlice := sl.LogRecords()
+	logSlice.EnsureCapacity(1)
+	record := logSlice.AppendEmpty()
+	dest := record.Body()
+	toAttributeMap(event.Object).CopyTo(dest)
+	return out
+
+}
+
+func unstructuredListToLogData(event *unstructured.UnstructuredList) plog.Logs {
+	out := plog.NewLogs()
+	rls := out.ResourceLogs().AppendEmpty()
+	sl := rls.ScopeLogs().AppendEmpty()
+	logSlice := sl.LogRecords()
+	logSlice.EnsureCapacity(len(event.Items))
+	for _, e := range event.Items {
+		record := logSlice.AppendEmpty()
+		dest := record.Body()
+		toAttributeMap(e.Object).CopyTo(dest)
+	}
+	return out
+}
+
+// Copied from https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/main/pkg/stanza/adapter/converter.go
+func toAttributeMap(obsMap map[string]interface{}) pcommon.Value {
+	attVal := pcommon.NewValueMap()
+	attMap := attVal.MapVal()
+	insertToAttributeMap(obsMap, attMap)
+	return attVal
+}
+
+func insertToAttributeMap(obsMap map[string]interface{}, dest pcommon.Map) {
+	dest.EnsureCapacity(len(obsMap))
+	for k, v := range obsMap {
+		switch t := v.(type) {
+		case bool:
+			dest.InsertBool(k, t)
+		case string:
+			dest.InsertString(k, t)
+		case []string:
+			arr := toStringArray(t)
+			dest.Insert(k, arr)
+		case []byte:
+			dest.InsertBytes(k, pcommon.NewImmutableByteSlice(t))
+		case int64:
+			dest.InsertInt(k, t)
+		case int32:
+			dest.InsertInt(k, int64(t))
+		case int16:
+			dest.InsertInt(k, int64(t))
+		case int8:
+			dest.InsertInt(k, int64(t))
+		case int:
+			dest.InsertInt(k, int64(t))
+		case uint64:
+			dest.InsertInt(k, int64(t))
+		case uint32:
+			dest.InsertInt(k, int64(t))
+		case uint16:
+			dest.InsertInt(k, int64(t))
+		case uint8:
+			dest.InsertInt(k, int64(t))
+		case uint:
+			dest.InsertInt(k, int64(t))
+		case float64:
+			dest.InsertDouble(k, t)
+		case float32:
+			dest.InsertDouble(k, float64(t))
+		case map[string]interface{}:
+			subMap := toAttributeMap(t)
+			dest.Insert(k, subMap)
+		case []interface{}:
+			arr := toAttributeArray(t)
+			dest.Insert(k, arr)
+		default:
+			dest.InsertString(k, fmt.Sprintf("%v", t))
+		}
+	}
+}
+
+func toAttributeArray(obsArr []interface{}) pcommon.Value {
+	arrVal := pcommon.NewValueSlice()
+	arr := arrVal.SliceVal()
+	arr.EnsureCapacity(len(obsArr))
+	for _, v := range obsArr {
+		insertToAttributeVal(v, arr.AppendEmpty())
+	}
+	return arrVal
+}
+
+func insertToAttributeVal(value interface{}, dest pcommon.Value) {
+	switch t := value.(type) {
+	case bool:
+		dest.SetBoolVal(t)
+	case string:
+		dest.SetStringVal(t)
+	case []string:
+		toStringArray(t).CopyTo(dest)
+	case []byte:
+		dest.SetBytesVal(pcommon.NewImmutableByteSlice(t))
+	case int64:
+		dest.SetIntVal(t)
+	case int32:
+		dest.SetIntVal(int64(t))
+	case int16:
+		dest.SetIntVal(int64(t))
+	case int8:
+		dest.SetIntVal(int64(t))
+	case int:
+		dest.SetIntVal(int64(t))
+	case uint64:
+		dest.SetIntVal(int64(t))
+	case uint32:
+		dest.SetIntVal(int64(t))
+	case uint16:
+		dest.SetIntVal(int64(t))
+	case uint8:
+		dest.SetIntVal(int64(t))
+	case uint:
+		dest.SetIntVal(int64(t))
+	case float64:
+		dest.SetDoubleVal(t)
+	case float32:
+		dest.SetDoubleVal(float64(t))
+	case map[string]interface{}:
+		toAttributeMap(t).CopyTo(dest)
+	case []interface{}:
+		toAttributeArray(t).CopyTo(dest)
+	default:
+		dest.SetStringVal(fmt.Sprintf("%v", t))
+	}
+}
+
+func toStringArray(strArr []string) pcommon.Value {
+	arrVal := pcommon.NewValueSlice()
+	arr := arrVal.SliceVal()
+	arr.EnsureCapacity(len(strArr))
+	for _, v := range strArr {
+		insertToAttributeVal(v, arr.AppendEmpty())
+	}
+	return arrVal
+}
